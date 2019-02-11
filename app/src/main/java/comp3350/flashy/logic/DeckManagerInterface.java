805cb1cb0ed5334fe1fc3b61e5c8f30404e586cd
sanbud <budhias@myumanitoba.ca>
2019-02-11 13:49:10 -0600

    merge attempt #2

--- app/src/main/java/comp3350/flashy/logic/DeckManagerInterface.java
+++
@@ -1,9 +0,0 @@
-package comp3350.flashy.logic;
-
-public interface DeckManagerInterface {
-
-    void addDeck(String deckName, DeckInterface newDeck);
-
-    void deleteDeck(String deckNameSelected);
-
-}
