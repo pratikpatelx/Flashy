681299938ce53a3fdbfb32edf55f5ca659dc54b7
duquek <duquek@myumanitoba.ca>
2019-03-02 10:44:23 -0600

    adding new gui for iteration 2

--- app/src/main/java/comp3350/flashy/presentation/MainActivity.java
+++ app/src/main/java/comp3350/flashy/presentation/MainActivity.java
@@ -1,50 +1,37 @@
 package comp3350.flashy.presentation;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import comp3350.flashy.R;
-import comp3350.flashy.logic.uiHandler;
-
-
 
 public class MainActivity extends AppCompatActivity {
-    private Button giveAccess;
-    public static uiHandler ui = new uiHandler();
 
+    private TextView Info;
+    private EditText Username;
+    private EditText Password;
+    private Button Enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ui.addStub(); //added sample stub for testing
-        giveAccess = (Button)findViewById(R.id.login_button);
-        giveAccess.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openCreateDeckActivity();
-                System.out.println("Log in Button CLicked");
+
+        Info = (TextView)findViewById(R.id.tvMainLogin);
+        Username = (EditText)findViewById(R.id.etMainUsername);
+        Password = (EditText)findViewById(R.id.etMainPassword);
+        Enter = (Button)findViewById(R.id.btnMainEnter);
+
+        Enter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
             }
         });
     }
-
-
-    public static uiHandler getHandler() {
-
-        return ui;
-    }
-    public void openCreateDeckActivity(){
-        Intent intent = new Intent(this, CreateDeckActivity.class);
-        startActivity(intent);
-    }
-
-
 }
-
