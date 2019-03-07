d130e57c9721d8281201d9688bcfe8fdb702289a
M <arjunwam@myumanitoba.ca>
2019-03-07 13:43:08 -0600

    HSQLDB added as an embedded DB

--- app/src/main/java/comp3350/flashy/persistence/DatabaseStub.java
+++ app/src/main/java/comp3350/flashy/persistence/DatabaseStub.java
@@ -1,5 +1,8 @@
 package comp3350.flashy.persistence;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Hashtable;
+import java.util.Set;
 
 import comp3350.flashy.DomainLogic.Deck;
 
@@ -10,13 +13,32 @@ public class DatabaseStub implements DatabaseImplementation{
         storage = new Hashtable();
     }
 
+    @Override
     public void inputDeck(String identifier, Deck inputDeck) {
         storage.put(identifier, inputDeck);
     }
 
+    @Override
     public Deck getDeck(String identifier) {
         Deck result = storage.get(identifier);
         storage.remove(identifier);
+        return result;
+    }
+
+    @Override
+    public Collection getDeckCollection() {
+        Collection result = null;
+        ArrayList temp = new ArrayList();
+
+        /*
+        get each key in the hashtable and put the corresponding Deck into an arraylist
+         */
+        Set<String> keys = storage.keySet();
+        for(String key: keys){
+            temp.add(storage.get(key));
+        }
+
+        result.add(temp);
         return result;
     }
 }
