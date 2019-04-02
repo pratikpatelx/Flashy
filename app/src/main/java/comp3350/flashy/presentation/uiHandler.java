51f473c81010d3908892b466e80513c84965ea66
pratikpatelx <33759282+pratikpatelx@users.noreply.github.com>
2019-04-02 10:47:55 -0500

    HOT FIX

--- app/src/main/java/comp3350/flashy/presentation/uiHandler.java
+++ app/src/main/java/comp3350/flashy/presentation/uiHandler.java
@@ -32,7 +32,7 @@ public class uiHandler {
     public void saveCard(String head, String content, int type) {
         switch(type){
             case 0: logic.putFlashcardInDeck(username, deckName,new Flashcard((deckName +"-"+ deckSize), head, content));
-            //deckSize++;
+            deckSize++;
             break;
             case 1: logic.putFlashcardInDeck(username, deckName,new FillInTheBlanksFlashcard((deckName +"-"+ deckSize), head, content," "));
             break;
